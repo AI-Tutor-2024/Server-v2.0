22df157896894369b26c79b065073acc9173f8db
EunbeenDev <ros4312@naver.com>
2024-09-13 17:38:55 +0900

    [FIX] summary 서버 분리에 따른 파일 삭제 반영
    speech yml 공백 -> 삭제

---
+++ src/main/java/com/example/ai_tutor/domain/summary/dto/response/SummaryRes.java
@@ -0,0 +1,13 @@
+package com.example.ai_tutor.domain.summary.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+//
+//@Getter
+//@Builder
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class SummaryRes {
+//}
