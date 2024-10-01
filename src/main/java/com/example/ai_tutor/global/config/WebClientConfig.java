a3bb6ee17f541b2e59e7920e2b642fb7df941828
hyunbin1 <kimgusqls1@gmail.com>
2024-10-01 19:01:30 +0900

    [REFACTORING] 이전 파이썬 코드에서 호출한 clova API를 도입하여 WebClientConfig 도 수정함. 더이상 python server ip 받지 않음

--- src/main/java/com/example/ai_tutor/global/config/WebClientConfig.java
+++ src/main/java/com/example/ai_tutor/global/config/WebClientConfig.java
@@ -3,27 +3,31 @@ package com.example.ai_tutor.global.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ReactorResourceFactory;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
 
-    @Value("${webclient.base-url}")
-    private String baseUrl;
+    @Value("${openapi.chatgpt.url}")
+    private String gptUrl;
+
+    @Value("${openapi.clova.url}")
+    private String clovaUrl;
 
     @Bean
-    public ReactorResourceFactory resourceFactory() {
-        ReactorResourceFactory factory = new ReactorResourceFactory();
-        factory.setUseGlobalResources(false);
-        return factory;
+    public WebClient gptWebClient() {
+        return createWebClient(gptUrl);
     }
 
     @Bean
-    public WebClient webClient(){
+    public WebClient clovaWebClient() {
+        return createWebClient(clovaUrl);
+    }
+
+    private WebClient createWebClient(String baseUrl) {
         return WebClient.builder()
                 .baseUrl(baseUrl)
                 .build();
     }
-
 }
+
