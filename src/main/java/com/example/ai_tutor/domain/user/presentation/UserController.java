61020ed0984807419a80ed4dacf4e2b1ecf2605a
EunbeenDev <ros4312@naver.com>
2024-10-03 00:21:19 +0900

    [FIX] 사용자 정보 조회 url 수정

--- src/main/java/com/example/ai_tutor/domain/user/presentation/UserController.java
+++ src/main/java/com/example/ai_tutor/domain/user/presentation/UserController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "User", description = "User API")
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/v1/user")
 public class UserController {
 
     private final UserService userService;
