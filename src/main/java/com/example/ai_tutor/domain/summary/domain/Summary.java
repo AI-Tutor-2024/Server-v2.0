b1356ea21bce77dfcbb6c3a44ffee029c4085bd5
EunbeenDev <ros4312@naver.com>
2024-10-04 23:19:10 +0900

    [FEAT] Summary 엔티티 생성

---
+++ src/main/java/com/example/ai_tutor/domain/summary/domain/Summary.java
@@ -0,0 +1,35 @@
+package com.example.ai_tutor.domain.summary.domain;
+
+
+import com.example.ai_tutor.domain.common.BaseEntity;
+import com.example.ai_tutor.domain.note.domain.Note;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="Summary")
+@NoArgsConstructor
+@Getter
+public class Summary extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="summary_id", updatable = false)
+    private Long summaryId;
+
+    @Lob
+    @Column(name="content")
+    private String content;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="note_id")
+    private Note note;
+
+    @Builder
+    public Summary(String content, Note note) {
+        this.content = content;
+        this.note = note;
+    }
+
+}
