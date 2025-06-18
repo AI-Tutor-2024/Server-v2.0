07e9d3402b9e0f1e471c3a4cbb292ba99427fc82
hyunbin1 <kimgusqls1@gmail.com>
2025-06-18 21:10:49 +0900

    [FIX] Error 코드 추가

--- src/main/java/com/example/ai_tutor/global/payload/ErrorCode.java
+++ src/main/java/com/example/ai_tutor/global/payload/ErrorCode.java
@@ -1,22 +1,24 @@
 package com.example.ai_tutor.global.payload;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
 
-    INVALID_PARAMETER(400, null, "잘못된 요청 데이터 입니다."),
-    INVALID_REPRESENTATION(400, null, "잘못된 표현 입니다."),
-    INVALID_FILE_PATH(400, null, "잘못된 파일 경로 입니다."),
-    INVALID_OPTIONAL_ISPRESENT(400, null, "해당 값이 존재하지 않습니다."),
-    INVALID_CHECK(400, null, "해당 값이 유효하지 않습니다."),
-    INVALID_AUTHENTICATION(400, null, "잘못된 인증입니다.");
+    INVALID_PARAMETER(HttpStatus.BAD_REQUEST, null, "잘못된 요청 데이터 입니다."),
+    INVALID_REPRESENTATION(HttpStatus.BAD_REQUEST, null, "잘못된 표현 입니다."),
+    INVALID_FILE_PATH(HttpStatus.BAD_REQUEST, null, "잘못된 파일 경로 입니다."),
+    INVALID_OPTIONAL_IS_PRESENT(HttpStatus.BAD_REQUEST, null, "해당 값이 존재하지 않습니다."),
+    INVALID_CHECK(HttpStatus.BAD_REQUEST, null, "해당 값이 유효하지 않습니다."),
+    INVALID_AUTHENTICATION(HttpStatus.BAD_REQUEST, null, "잘못된 인증입니다."),
+    PRACTICE_NOT_FOUND(HttpStatus.NOT_FOUND, null, "");
 
     private final String code;
     private final String message;
-    private final int status;
+    private final HttpStatus status;
 
-    ErrorCode(final int status, final String code, final String message) {
+    ErrorCode(final HttpStatus status, final String code, final String message) {
         this.status = status;
         this.message = message;
         this.code = code;
